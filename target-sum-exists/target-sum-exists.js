cfc0919fac0f6147c591e26b0308ac105a49283f
Xpwnstar78 <xpwnstar78@gmail.com>
2020-11-04 22:57:51 -0500

    initial commit

--- target-sum-exists/target-sum-exists.js
+++ target-sum-exists/target-sum-exists.js
@@ -10,3 +10,14 @@ module.exports = {
   targetSumExistsInTwoNumbers,
   targetSumExistsInThreeNumbers
 }
+
+const targetSumExistsInTwoNumbers = (list, target) => {
+  for (let i = 0; i < list.length; i++){
+    for (let j = i + 1; j < list.length; j++){
+      if (list[i] + list[j] === target) {
+        return true
+      }
+    }
+  }
+  return false
+}
